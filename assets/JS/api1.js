dcac95351e983af6a3d9cb1f1273c50a2e6efc09
unknown <mcsalazar122@gmail.com>
2022-06-09 22:44:42 -0500

    starter apiEvent code

--- assets/JS/api1.js
+++ assets/JS/api1.js
@@ -1,1 +1,14 @@
-var apiEvent = key
+var apiEvent = "https://app.ticketmaster.com/discovery/v2/"
+var nokeyapi = "https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale="
+
+fetch(nokeyapi)
+.then(function(response) {
+    return response.json();
+    console.log(response);
+})
+.then(function(event) {
+    console.log(event);
+   // displayEvent(event, embedded);
+    
+    
+})
