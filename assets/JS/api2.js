2406775a0aa4d664c9e1fdbe760e5240a4913a79
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-06-11 14:09:06 -0500

    weather-Displays

--- assets/JS/api2.js
+++ assets/JS/api2.js
@@ -8,6 +8,28 @@ var weather = {
         this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    var { name } = data;
+    var { icon, description } = data.weather[0];
+    var { temp, humidity } = data.main;
+    var { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText =
+      "Description:" + description + "";
+    document.querySelector(".temp").innerText = "Temp: " + temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed:" + speed + " km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".input").value);
   },
 };
+document.querySelector(".box button").addEventListener("click", function () {
+  weather.search();
+});
