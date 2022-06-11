da30e3a30ef078721b991c8b5b9b80062702ac06
unknown <mcsalazar122@gmail.com>
2022-06-11 16:10:53 -0500

    event update

--- assets/JS/api1.js
+++ assets/JS/api1.js
@@ -3,44 +3,49 @@
 var searchBtnEl = document.querySelector(".button");
 var cityInputEl = document.querySelector(".input");
 var dateInputEl = document.querySelector(".input-2");
+var eventContainerEl = document.querySelector(".event-container")
 
 
 var eventHandler = function (event) {
     event.preventDefault();
-    var city = cityInputEl.value.trim(); 
-    console.log(city);
-    if (city) {
+var cityName = cityInputEl.value.trim(); 
+console.log(cityName);
+    if (cityName) {
         
-        getEventData(city);
-        eventContainerEl.textContent = "";
-        cityInputEl.value = "";
+        //getEventData(cityName);
+       
     }
     else {
         alert("No events found - Enjoy the weather!")
     }
+var date = dateInputEl.value.trim();
+    console.log(date);
 
-};
-//,StartDateTime={}
-var getEventData = function (city) {
+
+
+ //var getEventData = function () {
     
-    console.log(city)
-    var apiEvent = "'https://app.ticketmaster.com/discovery/v2/events.json?city={}&apikey=kDANs259nX5PnjGOMkkA0AAh72DAKY2C&'" + city1 + '"';
+//     console.log(city)  
+    var apiEvent = `https://app.ticketmaster.com/discovery/v2/events.json?city=${cityName},StartDateTime=${date}&apikey=kDANs259nX5PnjGOMkkA0AAh72DAKY2C`
+
 
     fetch(apiEvent)
-        .then(function (response) {
-            return response.json();
+        .then(response => response.json())
+        .then(data => console.log(data));
+            //return response.json();
             
-        })
-        .then(function (event) {
-            console.log(event);
+        
+        //.then(function (event) {
+        
             //displayEvent(event, city);
             //error code here
-        });
+      //  });
 
     // var displayEvent = function (event, searchTerm) {
-    //searchTermEl.textContent = event.embedded.events
-    // }
-}
+//     //searchTermEl.textContent = event.embedded.events
+    }
+// //}
 
-searchBtnEl.addEventListener("submit", eventHandler);
+searchBtnEl.addEventListener("click", eventHandler);
 
+//}
