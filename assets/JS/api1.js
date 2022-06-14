940b306274609d5cd0806e4137b3ab2ad3d72538
unknown <mcsalazar122@gmail.com>
2022-06-14 18:20:41 -0500

    add links

--- assets/JS/api1.js
+++ assets/JS/api1.js
@@ -1,11 +1,10 @@
-//var nokeyapi = "https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=" + city;
-
 var searchBtnEl = document.querySelector(".startButton");
 var cityInputEl = document.querySelector(".input");
 var dateInputEl = document.querySelector(".input-2");
 var eventContainerEl = document.querySelector(".event-container");
 var eventNameEl = document.getElementById("eventName");
 var eventSeatingEl = document.querySelector(".seating");
+var pageBtnEl = document.querySelector("#page-[i]")
 
 //capture city and date for api search
 var eventHandler = function (event) {
@@ -13,13 +12,13 @@ var eventHandler = function (event) {
     var cityName = cityInputEl.value.trim();
     console.log(cityName);
     if (cityName) {
-       // getEventData(cityName);
+       getEventData(cityName);
     } else {
         alert("Please enter a US city");
     }
 };
 
-//var getEventData = function (cityName) {
+var getEventData = function (cityName) {
     
     var date = dateInputEl.value.trim();
     console.log(date);
@@ -51,27 +50,17 @@ var eventHandler = function (event) {
                 var eventGenre = eventData[i].classifications[0].genre.name;
                 document.querySelector(".genre").innerText = "Genre: " + eventGenre;
 
-                //  var eventGenre = eventData[i].classifications[0].genre.name;
-                //  document.querySelector(".genre").innerText = "Genre: " + eventGenre;
 
-                var eventSeating = eventData[i].seatmap.staticUrl;
-                var eventSeatingEl = document.getElementsByClassName("seating")
-                var eventSeatingEl = document.createElement("img")
-                document.querySelector(".seating").src = eventSeating;
+//remove not working
+               var eventSeating = eventData[i].seatmap.staticUrl;
+                console.log(eventSeating);
+               var eventSeatingEl = document.getElementsByClassName("seating")
+               document.querySelector(".seating").href = eventSeating;
 
 
                 var eventDescription = eventData[i].type;
                 document.querySelector(".description-1").innerText = "Description: " + eventDescription;
 
-                //Need help with "reading'0' " test code:
-
-                //var eventPrice = eventData[i]?.priceRanges[0]?.min
-
-                //  if (eventData[i].priceRanges === 0) {
-                //     document.querySelector(".price").innerText = "Prices start at: N/A";
-                //  } else {
-                //document.querySelector(".price").innerText = "Prices start at: " + eventPrice;
-                // end of help section
 
                 var eventTime = eventData[i].dates.start.localTime;
                 document.querySelector(".eventTimes").innerText = "Time: " + eventTime;
@@ -83,19 +72,25 @@ var eventHandler = function (event) {
 
 
                 var eventUrl = eventData[i].url;
-                var eventUrlEl = document.createElement('div')
-                eventUrlEl.setAttribute('class', ".link-event") = document.
-                    document.querySelector(".link").src = eventUrl;
+                document.querySelector(".event-link").href = eventUrl; //`href=${eventUrl}`;
 
-                // eventNameEl.textcontent = eventsName;
-
-                //       var eventTitleEl = document.createElement("p");
-                // eventTitleEl.classList = "genre";
-                // eventTitleEl.setAttribute("id", "eventName2");
-                // eventTitleEl.textContent = eventData[i].name;
+                //add data to cards
+                //pageBtnEl.addEventListener("click", pageHandler);
+                
+                
             }
                 //showEvents()
             });
-
+        };
 
 searchBtnEl.addEventListener("click", eventHandler);
+    //Need help with "reading'0' " test code:
+
+                // var eventPrice = eventData[i].priceRanges[0].min
+
+                //  if (eventData[i].priceRanges === 0) {
+                //     document.querySelector(".price").innerText = "Prices start at: N/A";
+                //  } else {
+                // document.querySelector(".price").innerText = "Prices start at: " + eventPrice;
+
+    // end of help section
